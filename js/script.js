8b90dd6c254d688ee6ce03a54228f6169bf43e85
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2011-04-21 13:19:02 -0500

    Draw commits inside a canvas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 (function($, undefined) {
 
+    var canvas = { x: 1200, y: 500 };
+    var cell = { x: 30, y: 30 };
+    var offset = { x: 10, y: 10 };
+
     var render = function(commits) {
-        $('#canvas').html("<p>" + commits.length + "</p>");
+        var paper = Raphael("canvas", canvas.x, canvas.y);
+
+        var frame = paper.path(createPath(
+            ["M",0,0],
+            ["L",canvas.x,0], ["L",canvas.x,canvas.y],
+            ["L",0,canvas.y], ["L",0,0]
+        )).attr({
+            stroke: "red",
+            'stroke-witdh': 5
+        });
+
+        _.each(commits, function(commit) {
+            paper.image("https://secure.gravatar.com/avatar/" + commit.gravatar,
+                commit.time * cell.x + offset.x,
+                commit.space * cell.y + offset.y,
+                10, 10
+            );
+        });
+    };
+
+    // Utils
+    var createPath = function() {
+        return _(arguments).chain()
+            .map(function(move) {
+                return [ move[0], move[1] + "," + move[2] ];
+            }).flatten().value().join('');
     };
 
     $(document).ready(function() {
