e9bcbc85face4c2e39ec2f7dd9ea71cf4f22e68c
Eduardo Lopez <eduardo.biagi@gmail.com>
2011-04-28 01:11:29 -0500

    Added camera.js

---
+++ public/js/camera.js
@@ -0,0 +1,53 @@
+window.camera = (function(_, undefined) {
+
+    var camera = { x: 0, y: 0 },
+        world = { x: 0, y: 0 };
+
+    var moveCamera = function(delta, callback) {
+        camera.x += delta;
+        if (!animate.running || animate.paused) {
+            moveWorld(callback);
+        }
+    };
+
+    // This should probably be in canvas.js so sets is not global
+    var moveWorld = function(callback) {
+        var delta = camera.x - world.x;
+        world.x = camera.x;
+
+        canvas.sets().world.translate(-delta, 0);
+
+        var minTime = (camera.x - 10) / dimensions.cell.w;
+        var maxTime = minTime + dimensions.canvas.w / dimensions.cell.w;
+
+        _.each(canvas.visible(), function(commit) {
+            var notVisible = commit.time < minTime || commit.time > maxTime;
+            if (notVisible) {
+                if (commit.avatar) {
+                    commit.avatar.remove();
+                }
+                _.each(commit.parents, function(parent) {
+                    parent.connection.remove(); 
+                });
+            }
+        });
+
+        if (callback) { callback(); }
+    };
+
+    return {
+        x: function() {
+            return camera.x;
+        },
+        y: function() {
+            return camera.y;
+        },
+        move: function(delta, callback) {
+            moveCamera(delta, callback);
+        },
+        moveWorld: function(callback) {
+            moveWorld(callback);
+        }
+    };
+
+})(window._);
